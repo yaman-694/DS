b957297dbc44d57f67c8949fcbc3271958abfa64
yaman <jyaman694@gmail.com>
2021-12-21 13:32:29 +0530

    reverse linkedList iterative method

--- linkedList/reverseLinkedList.cpp
+++ linkedList/reverseLinkedList.cpp
@@ -7,16 +7,30 @@ void reverse(Node *& head)
     Node * curr=head;
     Node* prev = NULL,*Next = head;
 
-    while(Next->next!=NULL)
+    while(curr!=NULL)
     {
+       Next = curr->next;
+       curr->next = prev;
+       prev = curr;
+       curr = Next;
        
-
-
+       
     }
+
+    head = prev;
+
+    
+
 }
 
 int main()
 {
-
+    Node * head = NULL;
+    insert(head,5);
+    insert(head,4);
+    insert(head,6);
+    insert(head,7);
+    reverse(head);
+    display(head);
     return 0;
 }
