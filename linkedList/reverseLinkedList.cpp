774b85f440f2ac40d13665248d42ddacca01f065
yaman <jyaman694@gmail.com>
2021-12-21 13:20:54 +0530

    reverse linked list

---
+++ linkedList/reverseLinkedList.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+#include"baseOfLinkedList.cpp"
+using namespace std;
+
+void reverse(Node *& head)
+{
+    Node * curr=head;
+    Node* prev = NULL,*Next = head;
+
+    while(Next->next!=NULL)
+    {
+       
+
+
+    }
+}
+
+int main()
+{
+
+    return 0;
+}
