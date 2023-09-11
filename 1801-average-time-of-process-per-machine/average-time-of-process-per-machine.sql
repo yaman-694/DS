de447095975d88338459c0c2ee341d2ddeb980e8
Yaman jain <72979343+yaman-694@users.noreply.github.com>
2023-09-11 20:05:40 +0530

    Time: 444 ms (77.99%) | Memory: 0B (100.00%) - LeetSync

--- 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
+++ 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -5,6 +5,6 @@ from activity a1,
 activity a2
 where a1.process_id = a2.process_id and a1.activity_type = 'start' and a2.activity_type='end' and a2.machine_id = a1.machine_id)
 
-select machine_id, Round(sum(time)/count(*),3) as processing_time
+select machine_id, Round(avg(time),3) as processing_time
 from cte
 group by machine_id
